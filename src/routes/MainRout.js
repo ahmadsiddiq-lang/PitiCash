1f6a56e9dd4d7a95ff4f7c54a96725324239d528
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-08-26 10:07:40 +0700

    tabbar

--- src/routes/MainRout.js
+++ src/routes/MainRout.js
@@ -1,72 +1,57 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function DetailsScreen() {
+function TestScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Details!</Text>
+            <Text>Test!</Text>
         </View>
     );
 }
 
-function HomeScreen({ navigation }) {
+function HomeScreen(props) {
+    const gotoTestStackScreen = () => {
+        props.navigation.navigate('Test');
+    };
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Details')}
-            />
+            <Text>Home!</Text>
+            <Button title="Go to test stack screen" onPress={gotoTestStackScreen} />
         </View>
     );
 }
 
-function SettingsScreen({ navigation }) {
+function SettingsScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Details')}
-            />
+            <Text>Settings!</Text>
         </View>
-    );
-}
-
-const HomeStack = createStackNavigator();
-
-function HomeStackScreen() {
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="Home" component={HomeScreen} />
-            <HomeStack.Screen name="Details" component={DetailsScreen} />
-        </HomeStack.Navigator>
-    );
-}
-
-const SettingsStack = createStackNavigator();
-
-function SettingsStackScreen() {
-    return (
-        <SettingsStack.Navigator>
-            <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-            <SettingsStack.Screen name="Details" component={DetailsScreen} />
-        </SettingsStack.Navigator>
     );
 }
 
 const Tab = createBottomTabNavigator();
 
+function MyTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+}
+
+const Stack = createStackNavigator();
+
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeStackScreen} />
-                <Tab.Screen name="Settings" component={SettingsStackScreen} />
-            </Tab.Navigator>
+            <Stack.Navigator initialRouteName="Tabs">
+                <Stack.Screen name="Test" component={TestScreen} />
+                <Stack.Screen name="Tabs" component={MyTabs} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
