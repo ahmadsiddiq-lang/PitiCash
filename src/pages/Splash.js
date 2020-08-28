cd4d18ce0dd72498e565bd1ff2197cad20917b4f
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-08-28 16:01:52 +0700

    splash

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -11,61 +11,109 @@ const data = [
     { image: require('../assets/images/splash/Gambar3.png'), title: 'Investasi Aman', text: 'Tidak perlu takut berinvestasi, kami menjamin uang anda kembali' },
 ];
 
-export default function Splash({ navigation }) {
-    const scrollRef = React.createRef();
-    const [indexOf, setIndex] = useState(0);
+// export default function Splash({ navigation }) {
+export default class Splash extends React.Component {
 
+    scrollRef = React.createRef();
+    constructor(props) {
+        super(props);
 
-    const handleSetIndex = (e) => {
+        this.state = {
+            indexOf: 0,
+        };
+    }
+
+    //setIndex
+    handleSetIndex = (e) => {
+        const viewSize = e.nativeEvent.layoutMeasurement.width;
         const contentOffset = e.nativeEvent.contentOffset.x;
-        const selectedIndex = Math.ceil(contentOffset / SCREEN_WIDTH);
-        // console.log(selectedIndex);
-        setIndex(selectedIndex);
+        const selectedIndex = Math.floor(contentOffset / viewSize);
+        this.setState({
+            indexOf: selectedIndex,
+        });
     };
 
+    _handleNext = () => {
+        this.setState(prev => ({ indexOf: prev.indexOf === data.length - 1 ? 0 : prev.indexOf + 1 }),
+            () => {
+                this.scrollRef.current.scrollTo({
+                    animatde: true,
+                    y: 0,
+                    x: SCREEN_WIDTH * this.state.indexOf,
+                });
+            }
+        );
+    }
 
+    // componentDidMount = () => {
+    //     this.interval = setInterval(() => {
+    //         this.setState(prev => ({ indexOf: prev.indexOf === data.length - 1 ? 0 : prev.indexOf + 1 }),
+    //             () => {
+    //                 this.scrollRef.current.scrollTo({
+    //                     animatde: true,
+    //                     y: 0,
+    //                     x: SCREEN_WIDTH * this.state.indexOf,
+    //                 });
+    //             }
+    //         );
+    //     }, 3000);
+    // }
 
-    return (
-        <View style={styles.Container}>
-            <ScrollView
-                horizontal
-                pagingEnabled
-                showsHorizontalScrollIndicator={false}
-                onMomentumScrollEnd={handleSetIndex}
-                ref={scrollRef}
-            >
-                {
-                    data.map((item, index) => {
-                        return (
-                            <View key={index} style={styles.BoxContent}>
-                                <View style={styles.BoxImage}>
-                                    <Image style={styles.Image} source={item.image} />
-                                </View>
-                                <View style={styles.BoxText}>
-                                    <Text style={{ fontSize: sizeFont(8), fontFamily: Poppins.Bold, color: color.mainColor }}>{item.title}</Text>
-                                    <Text style={{ textAlign: 'center', color: color.fontBody2 }}>{item.text}</Text>
-                                </View>
-                            </View>
-                        );
-                    })
-                }
-            </ScrollView>
-            <View style={styles.BoxFooter}>
-                <View style={styles.BoxCircle}>
+    // componentWillUnmount() {
+    //     // Clear the interval right before component unmount
+    //     clearInterval(this.interval);
+    // }
+
+    render() {
+        return (
+            <View style={styles.Container}>
+                <ScrollView
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator={false}
+                    onMomentumScrollEnd={this.handleSetIndex}
+                    ref={this.scrollRef}
+                >
                     {
                         data.map((item, index) => {
                             return (
-                                <View key={index} style={[styles.Circle, indexOf === index && styles.CicleActive]} />
+                                <View key={index} style={styles.BoxContent}>
+                                    <View style={styles.BoxImage}>
+                                        <Image style={styles.Image} source={item.image} />
+                                    </View>
+                                    <View style={styles.BoxText}>
+                                        <Text style={{ fontSize: sizeFont(8), fontFamily: Poppins.Bold, color: color.mainColor }}>{item.title}</Text>
+                                        <Text style={{ textAlign: 'center', color: color.fontBody2 }}>{item.text}</Text>
+                                    </View>
+                                </View>
                             );
                         })
                     }
+                </ScrollView>
+                <View style={styles.BoxFooter}>
+                    <View style={styles.BoxCircle}>
+                        {
+                            data.map((item, index) => {
+                                return (
+                                    <View key={index} style={[styles.Circle, this.state.indexOf === index && styles.CicleActive]} />
+                                );
+                            })
+                        }
+                    </View>
+                    {
+                        this.state.indexOf === data.length - 1 ?
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Tabs')} activeOpacity={0.6} style={styles.Btn}>
+                                <Text style={{ color: color.fontWhite, fontFamily: Poppins.Medium }}>Start</Text>
+                            </TouchableOpacity> :
+                            <TouchableOpacity onPress={() => this._handleNext()} activeOpacity={0.6} style={styles.Btn}>
+                                <Text style={{ color: color.fontWhite, fontFamily: Poppins.Medium }}>Next</Text>
+                            </TouchableOpacity>
+                    }
                 </View>
-                <TouchableOpacity activeOpacity={0.6} style={styles.Btn}>
-                    <Text style={{ color: color.fontWhite, fontFamily: Poppins.Medium }}>Next</Text>
-                </TouchableOpacity>
             </View>
-        </View>
-    );
+        );
+
+    }
 }
 
 const styles = StyleSheet.create({
